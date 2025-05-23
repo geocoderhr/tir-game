2ee58c7effa85d07ecf8ca2b52c00c81912dd1be
geocoderhr <degtiarevsergeidsn@gmail.com>
2025-05-23 13:09:04 +0200

    add final game

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import pygame
 import random
 
-
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HIGHT = 600
-screen = pygame.display.set_mode((SCREEN_HIGHT,SCREEN_HIGHT))
-
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
+pygame.display.set_icon(pygame.image.load("img/img.png"))
 
-icon = pygame.image.load("img\img.png")
-pygame.display.set_icon(icon)
+# Загрузка цели
+raw_target = pygame.image.load("img/09_49_34.png")
 
+# Задаём нужный размер
+target_width, target_height = 200, 200
+# Масштабируем изображение до 40×40
+target_img = pygame.transform.scale(raw_target, (target_width, target_height))
 
-target_img = pygame.image.load("img/09_49_34.png")
-target_width = 80
-target_height = 80
-target_x = random.randint(0,SCREEN_WIDTH - target_width)
-target_y = random.randint(0,SCREEN_HIGHT- target_width)
+# Теперь можно безопасно брать диапазон:
+target_x = random.randint(0, SCREEN_WIDTH  - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+# Случайный фон
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-color =(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+             mouse_x,mouse_y = pygame.mouse.get_pos()
+             if target_x < mouse_x < target_x + target_width and target_y< mouse_y < target_y + target_height:
+                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-
-pygame.display.set_mode()
-
-
-runnig = True
-while runnig:
-    pass
-
-
-
-
-
+    screen.fill(color)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
